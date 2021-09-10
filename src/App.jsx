a8732177062b7319c8ddc00d2f736e1cc2959e84
Hernani Gonzalez <chernani2003@gmail.com>
2021-09-10 12:47:45 -0500

    carousel, API recorride y econtrando valores

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import {BoxComponents} from './style/style'
 
 export default class App extends Component{
     render(){
+        // const lola = "marvel"
+        // const exp =  /v?/
+        // const exp2 = exp.exec(lola)
+        // console.log(exp2.input)
+
+        // fetch("https://api-block-master.herokuapp.com/Peliculas/").then((resp) => resp.json()).then((data) => {console.log(data[lola])})
+
         return(
             <BoxComponents>
                 <NavBarTop />
--- src/components/SectionCarrusel.jsx
+++ src/components/SectionCarrusel.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import ControlledCarousel from '../hook/carousel'
-// import {Carousel} from 'react-bootstrap'
-// import {CarouselLocal, CarouselLocalImg, BoxCarouselLocal} from '../style/style'
 
 export default class SectionCarrusel extends Component{
 
